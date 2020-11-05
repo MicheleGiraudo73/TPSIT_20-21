dd9de3886ea5df123a148b6b21bd01aa9f98df9e
MicheleGiraudo73 <72200866+MicheleGiraudo73@users.noreply.github.com>
2020-11-05 16:19:52 +0100

    Update spotify.c

--- Spotify/spotify.c
+++ Spotify/spotify.c
@@ -4,7 +4,6 @@
 #include <stdbool.h>
 #include <string.h>
 #define LUNG 30
-#define DIM 10
 #define LUNG_F 200
 
 typedef struct canzone{
@@ -14,38 +13,54 @@ typedef struct canzone{
 }Canzone;
 
 //funzione che randomizza le canzoni
-void canzoniRandom(Canzone ps[],int n){
+void canzoniRandom(Canzone* ps,int n){
     srand(time(NULL));
-    int vetR[DIM];
-    for(int k=0;k<n;k++){
-        vetR[k]=rand()%n;
-        for(int j=0;j<k;j++) {
-       		if(vetR[k]==vetR[j]) { //if che controlla che se ci sono dei valori uguali e se sono uguali si decrementa 
-            	   k--;
-            	   break;
+    int* vetR;
+    vetR = malloc(10 * sizeof(int));
+    for(int k=0; k < 10; k++){
+        *(vetR+k)=rand()%n;
+        for(int j=0;j < k; j++){
+            if(*(vetR+k) == *(vetR+j)){
+                k--;
+                break;
             }
         }
+        
     }
-    for(int k=0;k<n;k++){  //stampo le canzoni randomizzate
-        printf("%d-%s -%s\n",ps[vetR[k]].n,ps[vetR[k]].canzone,ps[vetR[k]].autore);
+    for(int j=0;j<n;j++){  //stampo le canzoni randomizzate
+        printf("%d- %s -%s\n",(ps+(*(vetR+j)))->n,(ps+(*(vetR+j)))->canzone,(ps+(*(vetR+j)))->autore);
     }
 }
 
+int nLinee(FILE *fp,char* linea){
+    int k;
+    while (fgets(linea,LUNG_F,fp)){
+        k++;
+    }
+    fclose(fp);
+    return k;
+}
+
 void main(){
-    Canzone playlist[DIM];  //vettore di strutture
+    Canzone* playlist;  //vettore di strutture
     FILE *fp;
+    char *linea;
+    linea = malloc(LUNG_F * sizeof(char));
     char nomeFileIn[]="canzoni.csv"; 
-    char linea[LUNG_F];
     int k=0;
     fp=fopen(nomeFileIn,"r");
     if(fp == NULL){
         printf("non esiste il file\n");
     }else{
+        k=nLinee(fp,linea);
+        fp=fopen(nomeFileIn,"r");
+        playlist = malloc(k * sizeof(Canzone));
+        k=0;
         //carico la struttura utilizzando fgets ovvero leggere una riga e poi spezzettarla per i vari campi
         while(fgets(linea,LUNG_F,fp)){
-            playlist[k].n=atoi(strtok(linea,","));
-            strcpy(playlist[k].canzone,strtok(NULL,","));
-            strcpy(playlist[k].autore,strtok(NULL,"\n"));
+            (playlist+k)->n = atoi(strtok(linea,","));
+            strcpy((playlist+k)->canzone,strtok(NULL,","));
+            strcpy((playlist+k)->autore,strtok(NULL,"\n"));
             k++;
         }
         canzoniRandom(playlist,k);  
